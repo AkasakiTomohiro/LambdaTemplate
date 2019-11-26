92e701646c17d3b23301236c188803722b37d0cb
t.akasaki <t.akasaki.csharp@gmail.com>
2019-11-26 21:51:32 +0900

    cdk追加

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['./src/**/*.ts', '!**/node_modules/**'],
+  collectCoverageFrom: ['./src/**/*.ts', './environment/**/*.ts', '!./environment/cfn.ts', '!**/node_modules/**'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
@@ -153,7 +153,7 @@ module.exports = {
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['node_modules/(inversify|inversify-express-utils)/.*'],
+  transformIgnorePatterns: ['node_modules/.*'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
